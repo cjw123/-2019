56da85e781ab006ca186ffb340c35bb7aa79c05f
cjw123 <1812260353@qq.com>
2019-04-21 12:45:30 +0800

    函数实现

--- 栈.cpp
+++ 栈.cpp
@@ -1,4 +1,5 @@
 #include<stdlib.h>
+#include<stdio.h>
 #define STACK_INIT_SIZE 100
 #define STACKINCREMENT 10
 typedef int SElemType;
@@ -42,4 +43,74 @@ Status pop(SqStack &s,SElemType &e)
     if(s.top==s.base) return false;
     e = * --s.top;
     return true;
+}
+
+Status StackEmpty(SqStack s)
+{
+    if(s.top==s.base) return true;
+    else return false;
+}
+void ClearStack(SqStack &s)
+{
+    s.top=s.base;
+}
+void DestroyStack(SqStack *s)
+{
+    free(s->base);
+    //free(s);
+}
+
+ SqStack s;
+ SElemType e;
+//十进制转换为8进制
+
+void TenTransfromToEight()
+{
+    int N;
+    InitStack(s);
+    scanf("%d",&N);
+    while(N)
+    {
+        push(s,N%8);
+        N = N / 8;
+    }
+    while(!StackEmpty(s))
+    {
+        pop(s,e);
+        printf("%d",e);
+    }
+}
+//行编辑程序
+void LineEdit()
+{
+    InitStack(s);
+    int c;
+    c = getchar();
+    while(c!=EOF)
+    {
+        while(c!=EOF&&c!='\n')
+        {
+            switch (c)
+            {
+                case '#':
+                    /* code */
+                    pop(s,c);
+                    break;
+                case '@':
+                    ClearStack(s);
+                default:
+                    push(s,c);break;
+            }
+            c = getchar();
+        }
+        ClearStack(s);
+        if(c!=EOF) c = getchar();
+    }
+    DestroyStack(&s);
+}
+int main()
+{
+   LineEdit();
+   return 0;
+
 }
